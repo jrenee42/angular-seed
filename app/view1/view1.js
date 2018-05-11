04cf4c014c33ebd39fed116cdc786f70e08a39a7
jrenee42 <jill.singer@nextstepliving.com>
2018-05-10 23:09:05 -0400

    has automatic deletion

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,15 +12,41 @@ angular.module('myApp.view1', ['ngRoute'])
     .controller('View1Ctrl', ['$scope', '$http',function($scope, $http) {
         $scope.hello='hello from jill 2';
 
+        var baseUrl = 'http://localhost:3001/users';
 
-$http.get('http://localhost:3001/users')
-        .success(function (response) {
-            $scope.users = response;
-        });
-
-
+        
+        $http.get(baseUrl)
+            .success(function (response) {
+                $scope.users = response;
+            });
+        
+        
         $scope.maybeDelete = function(id) {
             console.log('would pop up a dialog here to deletion......', id);
+            
+            
+            //first; just do it:
+            
+            
+            $http.delete(baseUrl + "/" + id).success(function(resp){
+                console.log('deletion response: ', resp);
+
+                //need to refresh the table:
+                $scope.users = $scope.users.filter(user => {
+                   return user.id !== id;
+                });
+
+                //$scope.users = $scope.users.filter(function(user){
+                 //   return user.id !== 
+                
+                
+            }).catch(function(err){
+                console.log('error...', err);
+            });
         };
 
-}]);
+
+
+
+        
+    }]);
